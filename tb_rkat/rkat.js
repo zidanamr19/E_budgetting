4b3bb28d0e74ea4578a956829c5a560a54c3e78b
zidanamr19 <zidanamr08@gmail.com>
2023-07-24 12:18:39 +0700

    ppp

--- tb_rkat/rkat.js
+++ tb_rkat/rkat.js
@@ -3,8 +3,39 @@ const router = express.Router();
 const database = require("../config/database")
 
 
+
+router.get("/", async (req, res) => {
+  try {
+    const rkatData = await database("tb_rkat")
+      .select("tb_rkat.*", "tb_renstra.program")
+      .leftJoin("tb_renstra", "tb_rkat.id_renstra", "tb_renstra.id_renstra");
+
+    if (rkatData.length > 0) {
+      return res.status(200).json({
+        status: 1,
+        message: "Data RKAT ditemukan",
+        rkat_data: rkatData,
+      });
+    } else {
+      return res.status(404).json({
+        status: 0,
+        message: "Data RKAT tidak ditemukan",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      status: 0,
+      message: error.message,
+    });
+  }
+});
+
+
+
+
+
 router.post("/simpan", async (req, res) => {
-    const { id_renstra, id_unit_kerja, tahun, baseline, status } = req.body;
+    const { id_renstra, id_unit_kerja, tahun, baseline, jumlah, status } = req.body;
   
     try {
       // Simpan data ke tabel tb_rkat
@@ -12,6 +43,7 @@ router.post("/simpan", async (req, res) => {
         id_renstra: id_renstra,
         id_unit_kerja: id_unit_kerja,
         tahun: tahun,
+        jumlah : jumlah,
         baseline : baseline,
         status: status,
         create_date: new Date(),
