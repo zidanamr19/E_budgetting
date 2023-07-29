dffe318fd54c8353d96103f4d5cc2deb5c80f869
zidanamr19 <zidanamr08@gmail.com>
2023-07-29 12:36:12 +0700

    plp

--- tb_program_kerja/program_kerja.js
+++ tb_program_kerja/program_kerja.js
@@ -2,18 +2,47 @@ const express = require("express");
 const router = express.Router();
 const database = require("../config/database");
 
+
+router.get("/", async (req, res) => {
+    try {
+      const prokerData = await database("tb_program_kerja")
+        .select("tb_program_kerja.*", "tb_rkat.tahun")
+        .leftJoin("tb_rkat", "tb_program_kerja.id_rkat", "tb_rkat.id_rkat");
+  
+      if (prokerData.length > 0) {
+        return res.status(200).json({
+          status: 1,
+          message: "Data proker ditemukan",
+          proker_data: prokerData,
+        });
+      } else {
+        return res.status(404).json({
+          status: 0,
+          message: "Data proker tidak ditemukan",
+        });
+      }
+    } catch (error) {
+      return res.status(500).json({
+        status: 0,
+        message: error.message,
+      });
+    }
+  });
+
+// Pastikan Anda telah menginisialisasi objek database dengan koneksi ke database menggunakan Knex
+
 router.post("/simpan", async (req, res) => {
     const { id_rkat, id_unit_kerja, nama_program_kerja, penjab, waktu_pelaksanaan, ploting_dana } = req.body;
     const status = "a";
     
     try {
-      // Simpan data ke tabel tb_program kerja
+      // Simpan data ke tabel tb_program_kerja
       const inputPROKER = {
         id_rkat: id_rkat,
         id_unit_kerja: id_unit_kerja,
         nama_program_kerja: nama_program_kerja,
-        penjab : penjab,
-        waktu_pelaksanaan : waktu_pelaksanaan,
+        penjab: penjab,
+        waktu_pelaksanaan: new Date(waktu_pelaksanaan), // Ubah string tanggal menjadi objek Date
         ploting_dana: ploting_dana,
         status: status,
         create_date: new Date(),
@@ -36,7 +65,7 @@ router.post("/simpan", async (req, res) => {
       });
     }
   });
-
+  
 
  
  module.exports = router
