7e908b47fd0be1d1ee1bfabd4ae7fc7f147f5065
zidanamr19 <zidanamr08@gmail.com>
2023-07-29 12:08:23 +0700

    uyuy

--- router/index.js
+++ router/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-router.use("/programkerja", require("../tb_program_kerja/program_kerja"))
+router.use("/proker", require("../tb_program_kerja/program_kerja"))
 router.use("/bidang_renstra", require("../tb_bidang_renstra/bidangrenstra"))
 router.use("/renstra", require("../tb_renstra/renstra"))
 router.use("/unit_kerja", require("../tb_unit_kerja/unit_kerja"))
