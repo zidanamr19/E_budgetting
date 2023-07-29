34841482c65d43a0c05f4e3d7efef4c106d3d79b
zidanamr19 <zidanamr08@gmail.com>
2023-07-29 12:05:09 +0700

    hhm

--- tb_program_kerja/program_kerja.js
+++ tb_program_kerja/program_kerja.js
@@ -2,6 +2,40 @@ const express = require("express");
 const router = express.Router();
 const database = require("../config/database");
 
+router.post("/simpan", async (req, res) => {
+    const { id_rkat, id_unit_kerja, nama_program_kerja, penjab, waktu_pelaksanaan, ploting_dana } = req.body;
+    const status = "a";
+    
+    try {
+      // Simpan data ke tabel tb_program kerja
+      const inputPROKER = {
+        id_rkat: id_rkat,
+        id_unit_kerja: id_unit_kerja,
+        nama_program_kerja: nama_program_kerja,
+        penjab : penjab,
+        waktu_pelaksanaan : waktu_pelaksanaan,
+        ploting_dana: ploting_dana,
+        status: status,
+        create_date: new Date(),
+        update_date: new Date(),
+      };
+      const [idPROKER] = await database("tb_program_kerja").insert(inputPROKER);
+  
+      return res.status(201).json({
+        status: 1,
+        message: "Berhasil",
+        result: {
+          id_program_kerja: idPROKER,
+          ...inputPROKER,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: 0,
+        message: error.message,
+      });
+    }
+  });
 
 
  
