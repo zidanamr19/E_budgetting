ebf75d469b8d205d7edd67ad1cbe150547313ccc
zidanamr19 <zidanamr08@gmail.com>
2023-08-07 00:33:43 +0700

    rkat

--- tb_rkat/rkat.js
+++ tb_rkat/rkat.js
@@ -4,24 +4,31 @@ const database = require("../config/database")
 
 
 
-router.get("/", async (req, res) => {
-  try {
-    const rkatData = await database("tb_rkat")
-      .select("tb_rkat.*", "tb_renstra.program")
-      .leftJoin("tb_renstra", "tb_rkat.id_renstra", "tb_renstra.id_renstra");
+router.get('/', async (req, res) => {
+  const { id_renstra, tahun } = req.query;
 
-    if (rkatData.length > 0) {
-      return res.status(200).json({
-        status: 1,
-        message: "Data RKAT ditemukan",
-        rkat_data: rkatData,
-      });
-    } else {
-      return res.status(404).json({
-        status: 0,
-        message: "Data RKAT tidak ditemukan",
+  try {
+    let query = database('tb_rkat')
+      .select('tb_rkat.id_renstra', 'tb_rkat.tahun', 'tb_renstra.program') // Ganti 'nama_program' dengan kolom yang sesuai pada tb_renstra
+      .leftJoin('tb_renstra', 'tb_rkat.id_renstra', 'tb_renstra.id_renstra'); // Ganti 'id_renstra' dan 'id' sesuai dengan kolom yang sesuai
+
+    // Jika id_renstra dan tahun tersedia, lakukan filter
+    if (id_renstra && tahun) {
+      query = query.where({
+        'tb_rkat.id_renstra': id_renstra,
+        'tb_rkat.tahun': tahun,
       });
     }
+
+    query = query.groupBy('tb_rkat.id_renstra', 'tb_rkat.tahun'); // Grup berdasarkan tahun dan id_renstra
+
+    const rkatData = await query;
+
+    return res.status(200).json({
+      status: 1,
+      message: 'Data berhasil diambil',
+      data: rkatData,
+    });
   } catch (error) {
     return res.status(500).json({
       status: 0,
@@ -31,11 +38,8 @@ router.get("/", async (req, res) => {
 });
 
 
-
-
-
 router.post("/simpan", async (req, res) => {
-  const { id_renstra, tahun, baseline, status, id_unit_kerja, jumlah } = req.body;
+  const {  id_renstra, tahun, baseline, status, id_unit_kerja, jumlah } = req.body;
 
   try {
     // Simpan data ke tabel tb_rkat
@@ -44,6 +48,7 @@ router.post("/simpan", async (req, res) => {
     // Simpan data id_unit_kerja dan jumlah ke dalam tabel tb_rkat
     for (let i = 0; i < id_unit_kerja.length; i++) {
       await database("tb_rkat").insert({
+
         id_renstra: id_renstra,
         tahun: tahun,
         baseline: baseline,
