786422de24dccf9db7112fddba512006333e6ae6
zidanamr19 <zidanamr08@gmail.com>
2023-08-07 12:22:46 +0700

    gygu

--- tb_rkat/rkat.js
+++ tb_rkat/rkat.js
@@ -36,13 +36,46 @@ router.get('/data-unit-kerja', async (req, res) => {
   }
 });
 
+router.get('/data-rkat/:id_unit_kerja', async (req, res) => {
+  const id_unit_kerja = req.params.id_unit_kerja;
+
+  try {
+    const rkatData = await database('tb_rkat')
+      .select(
+        'tb_rkat.id_rkat',
+        'tb_rkat.id_renstra',
+        'tb_rkat.id_unit_kerja',
+        'tb_rkat.tahun',
+        'tb_rkat.jumlah',
+        'tb_rkat.status',
+        'tb_unit_kerja.nama_unit_kerja',
+        'tb_renstra.program'
+      )
+      .leftJoin('tb_unit_kerja', 'tb_rkat.id_unit_kerja', 'tb_unit_kerja.id_unit_kerja')
+      .leftJoin('tb_renstra', 'tb_rkat.id_renstra', 'tb_renstra.id_renstra')
+      .where({
+        'tb_rkat.id_unit_kerja': id_unit_kerja,
+      });
+
+    return res.status(200).json({
+      status: 1,
+      message: 'Data RKAT berhasil diambil berdasarkan ID unit kerja',
+      data: rkatData,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 0,
+      message: error.message,
+    });
+  }
+});
 
 router.get('/', async (req, res) => {
   const { id_renstra, tahun } = req.query;
 
   try {
     let query = database('tb_rkat')
-      .select('tb_rkat.id_renstra', 'tb_rkat.tahun','tb_rkat.baseline', 'tb_renstra.program') // Ganti 'nama_program' dengan kolom yang sesuai pada tb_renstra
+      .select('tb_rkat.id_rkat','tb_rkat.id_renstra', 'tb_rkat.tahun','tb_rkat.baseline', 'tb_renstra.program') // Ganti 'nama_program' dengan kolom yang sesuai pada tb_renstra
       .leftJoin('tb_renstra', 'tb_rkat.id_renstra', 'tb_renstra.id_renstra'); // Ganti 'id_renstra' dan 'id' sesuai dengan kolom yang sesuai
 
     // Jika id_renstra dan tahun tersedia, lakukan filter
